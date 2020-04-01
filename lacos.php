306a50f5fad0e7c2ce34684fc92a92282ac543eb
guiCFA <62083584+guiCFA@users.noreply.github.com>
2020-04-01 19:23:41 -0300

    Create lacos.php

---
+++ lacos.php
@@ -0,0 +1,103 @@
+<?php
+
+ /*
+function fatorial($numero){
+	if($numero <= 1){
+		return $numero;
+	}else{
+		return $numero * fatorial($numero - 1);
+	}
+}
+
+//4*3*2 = 24
+echo fatorial(1);
+echo fatorial(3);
+echo fatorial(5);
+echo fatorial(7);
+echo fatorial(9);
+*/
+
+
+
+
+/*function corrente( ){-----------------------------------------
+    $soma = 0;
+    $contador = 0;
+    for($n = 50; $n <= 70; $n+=2){
+        $soma = $soma + $n;
+        $contador = $contador + 1;
+  
+    }
+   
+echo " a soma dos  números é $soma";
+echo " a média dos números é ".($soma/$contador);
+
+}
+
+
+
+corrente();
+*/
+
+
+
+/*---------------------------------------------------
+function  somar (){
+$n = [5, 3, 20, 55, 46, -50];
+$maior = 0;
+$menor = max($n);
+$media = 0;
+$contador = 0;
+
+
+for($i = 0; $i <=count($n); $i++){
+        if($n[$i] >= 0){
+            $contador = $contador +1;
+            if($n[$i] > $maior){
+                $maior = $n[$i];
+                //$menor = $maior;
+            }
+            if($n[$i] <= $menor){
+                $menor = $n[$i];
+            } 
+            $media = $media + $n[$i];
+        } else {
+        break;
+        }
+        
+        
+
+}
+echo "O maior número é $maior <br>";
+echo "O menor número é $menor <br>";
+echo "A média é ".$media/$contador;
+} 
+
+somar()
+*/
+
+
+
+
+/*
+  $base= 2;
+  $exp= 3;
+  
+  // FOR
+  for($resultado=1, $count=1 ; $count<=$exp ; $count++)
+   $resultado *= $base;
+  
+  echo "Resultado  : $resultado";
+  */
+ 
+
+
+
+
+
+
+
+
+
+
+?>
